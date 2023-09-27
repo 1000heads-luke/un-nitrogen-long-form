03b0e07662f58cc0c61aedea8eef0893315a1451
Luke Stevenson <luke.stevenson@1000heads.com>
2023-09-27 23:25:52 +1000

    Copy and Content Changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,11 @@
 // ===================
 // @prepros-prepend vendors/bootstrap.bundle.min.js
 // @prepros-prepend vendors/jquery-v3.6.1.js
+// @prepros-prepend vendors/gsap-3.9.1.min.js
+// @prepros-prepend vendors/scrollmagic-2.0.8.min.js
+// @prepros-prepend vendors/scrollmagic_plugin_gsap-2.0.8.min.js
+// @prepros-prepend vendors/scrollmagic_plugin_addindicators-2.0.8.min.js
+// @prepros-prepend vendors/owlcarousel2-2.3.4.min.js
 
 //
 // Components
